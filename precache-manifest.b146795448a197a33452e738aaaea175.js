51b424826894ab90ea05ee273f910d7a7a93bc49
M-Abdullah <m_abdullah05@outlook.com>
2019-11-09 17:26:01 +0500

    Updates

---
+++ precache-manifest.b146795448a197a33452e738aaaea175.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d41dd2fa632033b2d9ac6f628c42241",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "8a1e6caac9594fee74e6",
+    "url": "/robofriends/static/css/main.9f1e7e1b.chunk.css"
+  },
+  {
+    "revision": "7f82860ef3dce4d9acdc",
+    "url": "/robofriends/static/js/2.2a21f297.chunk.js"
+  },
+  {
+    "revision": "8a1e6caac9594fee74e6",
+    "url": "/robofriends/static/js/main.1c917a96.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
